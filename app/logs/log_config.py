8547c9a5e93e61f8ba396c9c1bd0fcc914c015b4
hafidz erdityo <hafidze15@gmail.com>
2025-03-19 09:27:39 +0700

    fix(log_config): add time rotating log

--- app/logs/log_config.py
+++ app/logs/log_config.py
@@ -1,41 +1,53 @@
 import structlog
 import logging
-from datetime import date
-import os
+from datetime import date, datetime
+import pytz
+from logging.handlers import TimedRotatingFileHandler
+
+# configure structlog
+def custom_time_stamper(_, __, event_dict):
+    event_dict['timestamp'] = datetime.now(pytz.timezone('Asia/Jakarta')).isoformat()
+    return event_dict
 
 structlog.configure(
     processors=[
         structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
         structlog.stdlib.PositionalArgumentsFormatter(),
+        custom_time_stamper,
         structlog.processors.JSONRenderer(),
     ],
     logger_factory=structlog.stdlib.LoggerFactory(),
 )
 
-log_file = f"logs/app_{date.today()}.log"
+# set up the log file
+log_file = f"logs/app-{date.today()}.log"
 
-# Create a file handler to log messages to the file
-file_handler = logging.FileHandler(log_file)
+# Custom formatter to adjust asctime to Indonesian time
+class CustomFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.fromtimestamp(record.created, pytz.timezone('Asia/Jakarta'))
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            s = dt.isoformat()
+        return s
 
-# Define a custom formatter that includes only the file name and lineno
-file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - [%(pathname)s:%(lineno)d] - %(message)s")
-file_handler.setFormatter(file_formatter)
+# Timed rotating file handler
+file_handler = TimedRotatingFileHandler("logs/app.log", when="midnight", interval=1)
+file_handler.setFormatter(CustomFormatter("%(asctime)s - %(levelname)s - %(message)s"))
+file_handler.suffix = "%Y-%m-%d"  # This will append the date to the log file name
 
-# Create a console handler to log messages to the console (stdout)
-console_handler = logging.StreamHandler()
+# stream handler (for terminal output)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(CustomFormatter("%(asctime)s - %(levelname)s - %(message)s"))
 
-# Define a custom formatter for the console with only the file name
-console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - [%(pathname)s:%(lineno)d] - %(message)s")
-console_handler.setFormatter(console_formatter)
-
-# Get the root logger
+# configure root logger
 root_logger = logging.getLogger()
-
-# Add both file and console handlers to the root logger
+root_logger.setLevel(logging.INFO)  # set the desired log level
 root_logger.addHandler(file_handler)
-root_logger.addHandler(console_handler)
-root_logger.setLevel(logging.INFO)
+root_logger.addHandler(stream_handler)
 
-
-# Create the structlog logger
+# get the structlog logger
 logger = structlog.get_logger()
+logger.info("Starting the application")
